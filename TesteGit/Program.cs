ca8e32b897f45ae705825e5576e2b7fba164aa8b
Kauane <kauane.marcolino22@gmail.com>
2023-10-06 09:27:41 -0400

    Primeiro commit

---
+++ TesteGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um numero: ");
+        double num = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Digite outo numero: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());     
+
+        double resultado = num + num1;
+
+        Console.WriteLine($"O resultado é: {resultado} ");
+
+        Console.ReadLine();
+    }
+}
